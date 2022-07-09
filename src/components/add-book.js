c49d86d615a07278c5c615ad275f04538c3b799d
AJmatos2 <amatos217@gmail.com>
2022-07-08 21:56:17 -0400

    added styling to form, built out nav bar and adadded the routing

--- src/components/add-book.js
+++ src/components/add-book.js
@@ -21,26 +21,30 @@ export default function addBook() {
   };
 
   return (
-    <div>
-      <h1>add a book!</h1>
-      <h1>{name}</h1>
-      <form onSubmit={postBook}>
+    <div className="add-book">
+      <h1 className="add-book-title">Enter your book's data!</h1>
+
+      <form onSubmit={postBook} className="add-book-form">
         <input
+          className="add-book-form-input"
           onChange={(event) => setName(event.target.value)}
           type="text"
           placeholder="name"
         />
         <input
+          className="add-book-form-input"
           onChange={(event) => setAuthor(event.target.value)}
           type="text"
           placeholder="author"
         />
         <input
+          className="add-book-form-input"
           onChange={(event) => setPrice(event.target.value)}
           type="number"
           placeholder="price"
         />
         <textarea
+          className="add-book-form-textarea"
           onChange={(event) => setDescription(event.target.value)}
           type="text"
           placeholder="description"
